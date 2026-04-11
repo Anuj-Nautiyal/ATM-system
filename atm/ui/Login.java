63ed7413be7e959fe749e120a0c5c8c1ffd61b5f
Arnav Thapliyal <thapliyalarnav77@gmail.com>
2026-04-12 01:14:15 +0530

    login-page-working

--- atm/ui/Login.java
+++ atm/ui/Login.java
@@ -1,15 +1,18 @@
 package atm.ui;
 
+import atm_backend.Conn;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 
-public class Login extends JFrame implements ActionListener{
+public class Login extends JFrame implements ActionListener {
     JButton submit, clear;
     JTextField cardField;
     JPasswordField pinField;
 
-    Login(){
+    Login() {
         setTitle("ATM-system");
         setLayout(null);
 
@@ -17,17 +20,17 @@ public class Login extends JFrame implements ActionListener{
         Image i1 = icon1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i2 = new ImageIcon(i1);
         JLabel label = new JLabel(i2);
-        label.setBounds(70,30,100,100);
+        label.setBounds(70, 30, 100, 100);
         add(label);
 
         JLabel greet = new JLabel("Welcome to DHAN Bank");
-        greet.setFont(new Font("Osward" , Font.BOLD, 38));
-        greet.setBounds(220,70,450,40);
+        greet.setFont(new Font("Osward", Font.BOLD, 38));
+        greet.setBounds(220, 70, 450, 40);
         add(greet);
 
         JLabel cardno = new JLabel("Enter Card Number:");
-        cardno.setFont(new Font("Raleway" , Font.BOLD, 25));
-        cardno.setBounds(70,200,250,40);
+        cardno.setFont(new Font("Raleway", Font.BOLD, 25));
+        cardno.setBounds(70, 200, 250, 40);
         add(cardno);
 
         cardField = new JTextField();
@@ -36,8 +39,8 @@ public class Login extends JFrame implements ActionListener{
         add(cardField);
 
         JLabel pin = new JLabel("Enter your PIN: ");
-        pin.setFont(new Font("Raleway" , Font.BOLD, 25));
-        pin.setBounds(70,270,250,40);
+        pin.setFont(new Font("Raleway", Font.BOLD, 25));
+        pin.setBounds(70, 270, 250, 40);
         add(pin);
 
         pinField = new JPasswordField();
@@ -47,15 +50,15 @@ public class Login extends JFrame implements ActionListener{
 
         submit = new JButton("SUBMIT");
         submit.setBounds(240, 350, 120, 35);
-        submit.setFont(new Font("Raleway" , Font.BOLD, 15));
+        submit.setFont(new Font("Raleway", Font.BOLD, 15));
         submit.setBackground(Color.black);
         submit.setForeground(Color.white);
         submit.addActionListener(this);
         add(submit);
-        
+
         clear = new JButton("CLEAR");
         clear.setBounds(390, 350, 120, 35);
-        clear.setFont(new Font("Raleway" , Font.BOLD, 15));
+        clear.setFont(new Font("Raleway", Font.BOLD, 15));
         clear.setBackground(Color.black);
         clear.setForeground(Color.white);
         clear.addActionListener(this);
@@ -66,15 +69,29 @@ public class Login extends JFrame implements ActionListener{
         setLocation(350, 200);
     }
 
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource() == submit){
-            setVisible(false);
-            new Menu().setVisible(true);
-        } else if (e.getSource() == clear){
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submit) {
+            Conn conn = new Conn();
+            String cardnumber = cardField.getText(); // get the card number from login page
+            String pinnumber = new String(pinField.getPassword()); // get password enterd in login page
+            String query = "select * from login where cardnumber = '" + cardnumber + "'and pinnumber = '" + pinnumber + "'";
+            try {
+                ResultSet rs = conn.s.executeQuery(query);
+                if (rs.next()) {
+                    setVisible(false);
+                    new Menu().setVisible(true);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect Card Number or Pin");
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        } else if (e.getSource() == clear) {
             cardField.setText("");
             pinField.setText("");
         }
     }
+
     public static void main(String[] args) {
         new Login();
     }
