3ae03ac0e8fef12a5539bdc7da0e14644cc2629f
Anuj <anujnautiyal393@gmail.com>
2026-04-12 15:11:09 +0530

    update the Menu class to MenuScreen

--- atm/ui/FastCashScreen.java
+++ atm/ui/FastCashScreen.java
@@ -79,7 +79,7 @@ public class FastCashScreen extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent ae){
         if(ae.getSource() == backButton){
             setVisible(false);
-            new Menu().setVisible(true);
+            new MenuScreen().setVisible(true);
         }
     }
     public static void main(String[] args) {
